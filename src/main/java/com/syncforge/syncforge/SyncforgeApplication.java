0f3b73e50da639b86870c6a0fbbd1e8352a9fc4f
Movindu Jayathilake <movidujayathilake@gmail.com>
2026-05-19 09:10:15 +0530

    Initial commit

---
+++ src/main/java/com/syncforge/syncforge/SyncforgeApplication.java
@@ -0,0 +1,13 @@
+package com.syncforge.syncforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncforgeApplication.class, args);
+	}
+
+}
