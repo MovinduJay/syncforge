ed977835066214ff31d5e539943b9ea3a53c4720
Movindu Jayathilake <movidujayathilake@gmail.com>
2026-05-27 20:31:33 +0530

    Added tenant repository

--- src/main/java/com/syncforge/syncforge/tenant/repository/TenantRepository.java
+++ src/main/java/com/syncforge/syncforge/tenant/repository/TenantRepository.java
@@ -1,4 +1,7 @@
 package com.syncforge.syncforge.tenant.repository;
 
-public class TenantRepository {
+import com.syncforge.syncforge.tenant.model.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
 }
